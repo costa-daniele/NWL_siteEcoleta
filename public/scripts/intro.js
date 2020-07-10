947107826489cba5219757db1654ea9e3d9fc692
costa-daniele <costadaniele.contato@gmail.com>
2020-07-09 21:47:23 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,78 @@
+//coments
+//document.write("Hello World")
+
+//variáveis, tipos de dados
+//var myvar
+//let myvar
+//myvar = "Hello World"
+//const myconst = "Olá Mundo"
+//document.write(myvar + myconst)
+
+//string
+/*const s1 = "Isso" 
+const s2 = ' é'
+const s3 =  ` uma string`
+
+document.write(s1 + s2 + s3)*/
+
+//number
+
+/*const n1 = '1'
+const n2 = 12
+document.write(n1 + n2)*/
+
+//boolean - true or false
+
+/*const bTrue = true
+const bFalse = false
+document.write(bFalse)*/
+
+//objeto, possuem propriedades e funcionalidade
+
+/*const pessoa = {
+  altura: "1,80m",
+  idade: 24,
+  nacionalidade: true,
+  correr(){
+    document.write("run forest")
+    return " run Forest"
+  }
+}
+
+document.write(pessoa.correr())*/
+
+// Array - Vetores
+
+/*const colecao = ["tênis", "camisa", "chapéu", 1, {name: "My Name"}]
+
+document.write(colecao[1])
+document.write(colecao[4].name)*/
+
+//funções - funcionalidade - atalhos - reuso de código
+
+//registrar função
+
+/*function sayMyName(name) {
+  document.write(name)
+}
+
+sayMyName("Dani")
+sayMyName(" Damile")*/
+
+//condicionais
+
+/*const notaFinal = 4
+
+if (notaFinal < 5) {
+  //caminho 1
+  document.write("Reprovado")
+} else {
+  //caminho 2
+  document.write("Aprovado")
+}*/
+
+//loop repetições
+
+/*for (i = 0; i < 10; i++) {
+  document.write(`<p>${i}</p>`)
+}*/
